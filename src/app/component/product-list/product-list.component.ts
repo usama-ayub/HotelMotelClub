97902ec480e1065474f79cbac3c450f7a19417ea
usama-ayub <usamaayub2012@gmail.com>
2020-08-14 04:01:37 +0500

    product list add

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const noUiSlider: any
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
+    console.log(noUiSlider)
+    var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [300, 700],
+    connect: true,
+    range: {
+        'min': 100,
+        'max': 1000
+    }
+});
   }
 
 }
