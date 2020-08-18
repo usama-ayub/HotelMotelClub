b0e0c9262299def6a7ecbe1fd5c4b69ffc780984
usama-ayub <usamaayub2012@gmail.com>
2020-08-18 14:19:15 +0500

    href remove and angular route add

--- src/app/component/term-condition/term-condition.component.ts
+++ src/app/component/term-condition/term-condition.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-term-condition',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermConditionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  routeTo(path:string){
+    this.router.navigate([path]);
   }
 
 }
