09780da81bc4462273e37e4c8759d0d73ae71dee
usama-ayub <usamaayub2012@gmail.com>
2020-08-22 16:36:25 +0500

    production build issue fixed

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  submittedRegister: boolean = false;
+  submittedLogin: boolean = false;
   isRequestLogin: boolean = false;
   constructor(
     private formBuilder: FormBuilder, 
@@ -33,6 +33,7 @@ export class LoginComponent implements OnInit {
   
 
     onSubmitLogin() : void {
+      this.submittedLogin = true;
       if (this.loginForm.invalid) {
           return;
       }
