18b26af8636485041dde52f34b7cdd6da1cf6e1c
usama-ayub <usamaayub2012@gmail.com>
2020-09-06 01:10:55 +0500

    unsubscribe feature implement

--- src/app/component/favourite/favourite.component.ts
+++ src/app/component/favourite/favourite.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/shared/services/product/product.service';
 import { CommonService } from 'src/app/shared/services/common/common.service';
 import { IFavouriteProduct, IFavouriteProductData } from 'src/app/interface/product';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-favourite',
   templateUrl: './favourite.component.html',
   styleUrls: ['./favourite.component.scss']
 })
-export class FavouriteComponent implements OnInit {
+export class FavouriteComponent implements OnInit, OnDestroy {
   private userId;
+  private unsubscribe$ = new Subject;
   pagination = {
     page:1,
     pageSize:3,
@@ -35,7 +38,9 @@ export class FavouriteComponent implements OnInit {
 
   getFavouriteProduct(data): void{
     let payload = {userId:this.userId,pageNumber:data.page,pageSize:data.pageSize};
-    this.productService.getFavouriteProduct(payload).subscribe((res)=>{
+    this.productService.getFavouriteProduct(payload)
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe((res)=>{
       this.favProduct = res;
       this.pagination.totalPages = Math.round((this.favProduct[0].total/this.pagination.pageSize));
     }, (error)=>{
@@ -46,7 +51,9 @@ export class FavouriteComponent implements OnInit {
     if(this.isRemoveFavorite) return;
     let payload:IFavouriteProduct = {userId:this.userId,adId:$event};
     this.isRemoveFavorite = true;
-    this.productService.removeFavouriteProduct(payload).subscribe((res)=>{
+    this.productService.removeFavouriteProduct(payload)
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe((res)=>{
       this.commonService.success('Ad has been removed from Favourite List');
       this.isRemoveFavorite = false;
       this.getFavouriteProduct(this.pagination);
@@ -56,4 +63,8 @@ export class FavouriteComponent implements OnInit {
     })
   }
 
+  ngOnDestroy(){
+      this.unsubscribe$.next();
+      this.unsubscribe$.complete();
+  }
 }
